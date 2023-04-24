9467fd5163c69a328fc637d8a1e70897e94435f8
Dimitri <61168895+816020479@users.noreply.github.com>
2023-04-24 18:01:19 -0400

    Models including: user, exercise and user progress

--- App/models/user.py
+++ App/models/user.py
@@ -4,32 +4,75 @@ from App.database import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False, unique=True)
+    username =  db.Column(db.String(255), nullable=False, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    weight = db.Column(db.Float)
 
+    #initiate user
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.set_password(password)
+        self.weight = None
 
+    #return user data
     def get_json(self):
-        return{
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
-        }
+          return {
+                    'id': self.id,
+                    'username': self.username,
+                    'email': self.email
+                }
 
-    #hashes the password parameter and stores it in the object
     def set_password(self, password):
         """Create hashed password."""
         self.password = generate_password_hash(password, method='sha256')
     
-    #Returns true if the parameter is equal to the object’s password property
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
-    
-    #To String method
+
     def __repr__(self):
-        return f'<User {self.id}: {self.username}: {self.email}>' 
+          return f'<User {self.id}: {self.username}>'
+
+
+
+#User progress for chosen workout
+class UserProgress(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    exercise_id =  db.Column(db.Integer, db.ForeignKey('excercise.id'), nullable=False)
+    exercise_name = db.Column(db.String(255), db.ForeignKey('excercise.name'), nullable=False)
+    progress_value = db.Column(db.Integer, default=0)
+
+    def __init__(self, user_id, progress_value=0):
+        self.user_id = user_id
+        self.progress_value = progress_value
+
+    def __repr__(self):
+        return f"<UserProgress {self.user_id} - {self.progress_value}>"
+    
+    def get_json(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'exercise_id': self.exercise_id,
+            'exercise_name': self.exercise_name,
+            'progress_value': self.progress_value
+        }
+
+#all Ecercises
+class Exercise(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+
+    def get_json(self):
+        return {
+                'exercise_id': self.id,
+                'name': self.name,
+                'description': self.description
+               }
+    
+    
+
